01dee3bdea44a70e3511934425d04bda3436cb51
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2019-05-02 14:56:45 +0200

    BrugerHandler klasse har fået en opretBruger

--- src/BrugerHandler.java
+++ src/BrugerHandler.java
@@ -1,6 +1,97 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class BrugerHandler {
     // KonkurrenceMedlem k1 = new KonkurrenceMedlem(21, "Flemming", true, true, "Butterfly", 100);
     // Motionist m1 = new Motionist(80, "Lotte",false, false);
+    Scanner scanner = new Scanner(System.in);
 
-    
+    public void opretMotionist(List<Motionist> motionister, List<KonkurrenceMedlem> konkurrencemedlemmer) {
+        System.out.println("Du er ved at lave brugere!");
+
+        int brugerType;
+        boolean again = true;
+
+        int age;
+        String navn;
+        boolean aktiv;
+        boolean restance;
+        String swinType;
+        int swimTid;
+
+        while (again) {
+            System.out.println("Hvilken type svømmer 1/2");
+            brugerType = scanner.nextInt();
+
+
+            System.out.print("age: ");
+            age = scanner.nextInt();
+            System.out.print("navn: ");
+            navn = scanner.next();
+
+            System.out.print("aktiv Ja/Nej: ");
+            aktiv = jaNej();
+
+            System.out.print("restance Ja/Nej: ");
+            restance = jaNej();
+
+            if (brugerType == 1) {
+                Motionist motionistListe = new Motionist(age, navn, aktiv, restance);
+                motionister.add(motionistListe);
+            } else if (brugerType == 2) {
+                KonkurrenceMedlem konkurrenceMedlemListe = new KonkurrenceMedlem(age, navn, aktiv, restance, "", 0);
+                konkurrencemedlemmer.add(konkurrenceMedlemListe);
+            } else {
+                System.out.println("Forkert type");
+            }
+
+
+            System.out.println("Vil du fortsætte? Ja/Nej");
+            again = jaNej();
+        }
+    }
+
+    public boolean jaNej() {
+        boolean returnValue;
+        String jaNej;
+        char jn;
+
+        do {
+            jaNej = scanner.next().toLowerCase();
+            jn = jaNej.charAt(0);
+        } while (jn != 'j' && jn != 'n');
+
+        if (jn == 'j') {
+            returnValue = true;
+        } else if (jn == 'n') {
+            returnValue = false;
+        } else {
+            System.out.println("Forkert input, prøv igen!");
+            returnValue = false;
+        }
+        return returnValue;
+    }
+
+    /*
+    public int brugerOpretMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int valg = scanner.nextInt();
+
+
+        switch (valg) {
+            case 1:
+                // Motionist
+
+                break;
+
+            case 2:
+                // Konkurrence
+
+                break;
+
+            default:
+                System.out.println("Du har dummet dig, idiot!");
+                break;
+        }
+    */
 }
--- src/Main.java
+++ src/Main.java
@@ -17,11 +17,16 @@ public class Main {
         // Util metoder
         PrisUdregner pris = new PrisUdregner();
         FilHandtering fil = new FilHandtering();
+        BrugerHandler bruger = new BrugerHandler();
+
+
+
 
         // Stævne oprettelse
         // Stævne s1 = new Stævne("EM i butterfly", "Lygten 37", 123);
 
         // Bruger oprettelse
+        /*
         KonkurrenceMedlem k1 = new KonkurrenceMedlem(21, "Flemming", true, true, "Butterfly", 100);
         KonkurrenceMedlem k2 = new KonkurrenceMedlem(15, "Lars", true, false, "Freestyle", 125);
         KonkurrenceMedlem k3 = new KonkurrenceMedlem(7, "Ib", true, false, "Freestyle",143);
@@ -30,11 +35,15 @@ public class Main {
         Motionist m2 = new Motionist(60, "Søren", true, true);
         Motionist m3 = new Motionist(44, "Else", true, false);
         Motionist m4 = new Motionist(50, "Henrik", false, true);
+        */
 
 
         // Opret og tilføj til List (array)
         List<Motionist> motionister = new ArrayList();
         List<KonkurrenceMedlem> konkurrencemedlemmer = new ArrayList<>();
+
+        bruger.opretMotionist(motionister, konkurrencemedlemmer);
+
 
         /*
         motionister.add(m1);
@@ -52,21 +61,25 @@ public class Main {
         fil.skrivFil(motionister, konkurrencemedlemmer);
 
         // Loop for alle motionister - Unsorted
+        System.out.println("Motionister:");
         for (Motionist m : motionister) {
             System.out.println(m.getId() +" " +m.getNavn() +" " +m.getAge() +" " +pris.getPris(m.isAktiv(), m.getAge()) +" DKK");
         }
 
         System.out.println("- - -");
-
+        System.out.println("KonkurrenceMedlemmer");
         for (KonkurrenceMedlem k : konkurrencemedlemmer) {
             System.out.println(k.getId() +" " +k.getNavn() +" " +k.getAge() +" " +pris.getPris(k.isAktiv(), k.getAge()) +" DKK " +k.getSwimType()+" "+k.getSwimTid());
         }
         // Sort
         // Collections.sort(motionister);
 
+        /*
         // Loop for alle motionister - Sorted
         for (Motionist m : motionister) {
             // print
+
         }
+        */
     }
 }
