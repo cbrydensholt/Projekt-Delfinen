4bc558aed6ebd8b00e93addf87afccc142a41046
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2019-05-02 12:59:24 +0200

    test
    vi tester igen

---
+++ src/BrugerHandler.java
@@ -0,0 +1,6 @@
+public class BrugerHandler {
+    // KonkurrenceMedlem k1 = new KonkurrenceMedlem(21, "Flemming", true, true, "Butterfly", 100);
+    // Motionist m1 = new Motionist(80, "Lotte",false, false);
+
+    
+}
